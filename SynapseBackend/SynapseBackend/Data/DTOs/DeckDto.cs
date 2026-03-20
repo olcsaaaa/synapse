b5e29f8e5933fe9fa04dc0a26fd040aaa8515e65
Balassa Olivér <balassa.oliver@outlook.com>
2026-03-20 22:38:52 +0100

    Introduce Flashcard entity, update Deck model, and implement DeckService.

--- SynapseBackend/SynapseBackend/Data/DTOs/DeckDto.cs
+++ SynapseBackend/SynapseBackend/Data/DTOs/DeckDto.cs
@@ -1,3 +1,12 @@
 ﻿namespace SynapseBackend.Data.DTOs;
 
-public record DeckDto(string Id, string Name, string Description, string OwnerId, int ColorSeed);
+
+public record DeckDto(
+    string Id, 
+    string Name, 
+    string Description, 
+    string OwnerId, 
+    int ColorSeed, 
+    long LastModified, 
+    bool IsDeleted = false
+);
